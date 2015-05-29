036f2465f704a890e5a1a33802543c6f7099089f
mhcodner <mhcodner@gmail.com>
2015-05-29 23:01:19 +1000

    Updated README and added class

---
+++ GoogleJsonWebToken.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Specialized;
+using System.IO;
+using System.Net;
+using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Web;
+using System.Web.Script.Serialization;
+
+public class GoogleJsonWebToken
+{
+    public const string SCOPE_CALENDAR = "https://www.googleapis.com/auth/calendar";
+
+    public static dynamic GetAccessToken(string clientIdEMail, string keyFilePath, string scope)
+    {
+        // certificate
+        var bytes = File.ReadAllBytes(keyFilePath);
+        var certificate = new X509Certificate2(keyFilePath, "notasecret", X509KeyStorageFlags.Exportable | X509KeyStorageFlags.MachineKeySet);
+
+        // header
+        var header = new { typ = "JWT", alg = "RS256" };
+
+        // claimset
+        var times = GetExpiryAndIssueDate();
+        var claimset = new
+        {
+            iss = clientIdEMail,
+            scope = scope,
+            aud = "https://accounts.google.com/o/oauth2/token",
+            iat = times[0],
+            exp = times[1],
+        };
+
+        JavaScriptSerializer ser = new JavaScriptSerializer();
+
+        // encoded header
+        var headerSerialized = ser.Serialize(header);
+        var headerBytes = Encoding.UTF8.GetBytes(headerSerialized);
+        var headerEncoded = Convert.ToBase64String(headerBytes);
+
+        // encoded claimset
+        var claimsetSerialized = ser.Serialize(claimset);
+        var claimsetBytes = Encoding.UTF8.GetBytes(claimsetSerialized);
+        var claimsetEncoded = Convert.ToBase64String(claimsetBytes);
+
+        // input
+        var input = headerEncoded + "." + claimsetEncoded;
+        var inputBytes = Encoding.UTF8.GetBytes(input);
+
+        // signature
+        var rsa = certificate.PrivateKey as RSACryptoServiceProvider;
+        var cspParam = new CspParameters
+        {
+            Flags = CspProviderFlags.UseMachineKeyStore,
+            KeyContainerName = rsa.CspKeyContainerInfo.KeyContainerName,
+            KeyNumber = rsa.CspKeyContainerInfo.KeyNumber == KeyNumber.Exchange ? 1 : 2
+        };
+        var aescsp = new RSACryptoServiceProvider(cspParam) { PersistKeyInCsp = false };
+        var signatureBytes = aescsp.SignData(inputBytes, CryptoConfig.MapNameToOID("SHA256"));
+        var signatureEncoded = Convert.ToBase64String(signatureBytes);
+
+        // jwt
+        var jwt = headerEncoded + "." + claimsetEncoded + "." + signatureEncoded;
+        System.Diagnostics.Debug.WriteLine(Encoding.UTF8.GetString(inputBytes));
+
+        var client = new WebClient();
+        client.Encoding = Encoding.UTF8;
+        var uri = "https://accounts.google.com/o/oauth2/token";
+        var content = new NameValueCollection();
+
+        content["assertion"] = jwt;
+        content["grant_type"] = "urn:ietf:params:oauth:grant-type:jwt-bearer";
+
+        string response = Encoding.UTF8.GetString(client.UploadValues(uri, "POST", content));
+
+        var result = ser.Deserialize<dynamic>(response);
+
+        return result;
+    }
+
+    private static int[] GetExpiryAndIssueDate()
+    {
+        var utc0 = new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc);
+        var issueTime = DateTime.UtcNow;
+
+        var iat = (int)issueTime.Subtract(utc0).TotalSeconds;
+        var exp = (int)issueTime.AddMinutes(55).Subtract(utc0).TotalSeconds;
+
+        return new[] { iat, exp };
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,44 @@
-# GoogleJsonWebToken
+## What does this do?
+This is a C# class that I modified from [this blog post][1].
+It is modified to use the `MachineKeySet` instead of the `UserKeySet`
+which allows this class to run on Azure Websites.
+
+To quote the original blog post, this is used
+> To support scenarios where an unattended application accesses Google data,
+Google introduced the concept of Service Accounts which allows for unattended
+log in using [JWT][2] (JSON Web Token).
+
+### Why the modification to `MachineKeySet`?
+The detailed answer can be found on [this blog post][3] but the TL;DR is that
+the user profile doesn't exist in Azure Websites.
+
+## How do I use it?
+First you need
+1. A Google account
+2. A Google Service Account with the appropriate API access for what you want
+to do. This can be obtained from the [Google Developer console][4].
+  * Create a Service Account from the `APIs & auth / Credentials` page. Take note
+   of the email address in the form of `foo@developer.gserviceaccount.com`.
+  * Download the p12 key for the Service Account. Preferably place it somewhere
+   in your project that isn't publicly accessible such as `~/App_Data/`.
+
+Then you can request an access token using
+```
+var auth = GoogleJsonWebToken.GetAccessToken(
+    "foo@developer.gserviceaccount.com",
+    Server.MapPath("~/App_Data/key.p12"),
+    GoogleJsonWebToken.SCOPE_CALENDAR);
+var token = auth["access_token"];
+
+// Then you can start making authorised API calls
+string response;
+using (WebClient client = new WebClient())
+{
+    response = client.DownloadString(String.Format("https://www.googleapis.com/calendar/v3/calendars/{0}/events?access_token={1}&singleEvents=true", CalendarId, token));
+}
+```
+ 
+ [1]: https://zavitax.wordpress.com/2012/12/17/logging-in-with-google-service-account-in-c-jwt/
+ [2]: http://tools.ietf.org/html/draft-ietf-oauth-json-web-token-05
+ [3]: http://blog.tylerdoerksen.com/2013/08/23/pfx-certificate-files-and-windows-azure-websites/
+ [4]: https://console.developers.google.com/
